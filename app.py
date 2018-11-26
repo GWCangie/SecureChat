b9656b93674eb45fe2dd2405a841cb89b22e8b10
dmitrijoseph <a.dmitrijoseph@gmail.com>
2018-11-26 05:41:48 -0500

    Added libsodium

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import nacl.secret
+import nacl.utils
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+import nacl.secret
+import nacl.utils
+
+# This must be kept secret, this is the combination to your safe
+key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+
+# This is your safe, you can use it to encrypt or decrypt messages
+box = nacl.secret.SecretBox(key)
+
 @app.route('/')
 def sessions():
-    return render_template('session.html')
+    return render_template('session.html', secret=key)
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
